31438ae21b7a8310f7a97af63708b5d3c779c094
Minjun Kim <mjaything@gmail.com>
2020-07-25 01:09:00 +0200

    feat: add html elements to album.js

--- src/Album.js
+++ src/Album.js
@@ -1,15 +1,25 @@
 import React from "react";
 import propTypes from "prop-types";
+import "./Album.css";
 
-function Album({ id, position, artist, title, cover }) {
-  return <h4>{title}</h4>;
+function Album({ rank, title, artist, cover }) {
+  return (
+    <div class="album">
+      <img src={cover} alt={title} title={title} />
+      <div class="album__info">
+        <h3 class="album__rank">{rank}</h3>
+        <h4 class="album__title">{title}</h4>
+        <h5 class="album__artist">{artist}</h5>
+      </div>
+    </div>
+  );
 }
 
 Album.propTypes = {
   id: propTypes.number.isRequired,
-  position: propTypes.number.isRequired,
-  artist: propTypes.string.isRequired,
+  rank: propTypes.number.isRequired,
   title: propTypes.string.isRequired,
+  artist: propTypes.string.isRequired,
   cover: propTypes.string.isRequired,
 };
 
