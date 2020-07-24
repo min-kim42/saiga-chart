ecb21159f60d302f116bc50cdbdd315f5860969f
Minjun Kim <mjaything@gmail.com>
2020-07-24 18:04:41 +0200

    feat: extend React component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import axios from "axios";
+import Album from "./Album";
 
-function App() {
-  return <div className="App" />;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    albums: [],
+  };
+  // wait until the album data is fully loaded
+  getAlbums = async () => {
+    const albums = await axios.get(
+      // use CORS proxy to get around "No Acess-Control-Allow-Origin header" error
+      "https://fierce-ridge-86043.herokuapp.com/https://api.deezer.com/chart/0/albums"
+    );
+    this.setState({ albums: albums.data.data, isLoading: false });
+  };
 }
 
 export default App;
