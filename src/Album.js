42e9fc8535ee0dd8aa8a72d038f0da88fc5027e8
Minjun Kim <mjaything@gmail.com>
2020-07-24 18:43:09 +0200

    feat: add proptypes to Album function

--- src/Album.js
+++ src/Album.js
@@ -1,7 +1,16 @@
 import React from "react";
+import propTypes from "prop-types";
 
 function Album({ id, position, artist, title, cover }) {
   return <h4>{title}</h4>;
 }
+
+Album.propTypes = {
+  id: propTypes.number.isRequired,
+  position: propTypes.number.isRequired,
+  artist: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  cover: propTypes.string.isRequired,
+};
 
 export default Album;
