a05858c01098295237ffcb9dca7edaca13bcd57a
Minjun Kim <mjaything@gmail.com>
2020-07-26 02:34:29 +0200

    Updates

---
+++ precache-manifest.b5eb72834ad7642ebbeca4581f627e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fba12dea4e417f263d955930aee9dab",
+    "url": "/saiga-chart/index.html"
+  },
+  {
+    "revision": "027a07e313877fb92738",
+    "url": "/saiga-chart/static/css/main.b2278133.chunk.css"
+  },
+  {
+    "revision": "0f2de2546fef189ee0de",
+    "url": "/saiga-chart/static/js/2.a91b5c68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/saiga-chart/static/js/2.a91b5c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027a07e313877fb92738",
+    "url": "/saiga-chart/static/js/main.94eeede1.chunk.js"
+  },
+  {
+    "revision": "2d63c59310ac3ed95c3c",
+    "url": "/saiga-chart/static/js/runtime-main.11bfe2e2.js"
+  },
+  {
+    "revision": "398b87c2bf204cf38196a4d2ce0cb51c",
+    "url": "/saiga-chart/static/media/deezer_logo.398b87c2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9e8b30bf0995e414ceb174cc6cd25c4.js"
+  "/saiga-chart/precache-manifest.b5eb72834ad7642ebbeca4581f627e85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saiga-chart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
